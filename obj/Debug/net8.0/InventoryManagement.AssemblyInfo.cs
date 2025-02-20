1cfb83db6de32047d6bcdb544c8619674447c7ce
Mike Sheets <wmichaelsheets@gmx.com>
2025-02-20 09:07:44 -0500

    Everything seems to be working. . .

--- obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc7ca22516179b01617e2031a0bc9cdac9a8db0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d81178ed6e2575aa6858a30860c1989e353c78c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
